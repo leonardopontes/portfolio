eb569b9466b8197422af9d01d6f0a4136f69102a
leonardopontes <firstrackmeninw6@gmail.com>
2023-10-12 13:20:31 -0300

    Home Construction ...

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,26 +1,29 @@
+"use client"
 import React from 'react';
+import Image from 'next/image'
+import { TypeAnimation } from 'react-type-animation';
 
 const Home = () => {
+  const work = ['Frontend Engineer', 2000, 'Backend Engineer', 2000]
 
   return (
-    <div className="w-11/12 mx-auto flex justify-center -mt-36 ms:-mt-10 py-20">
-      <div className="text-white py-20">
-        <h1 className="font-bold text-3xl ms:text-5xl text-center py-20"> Filmes, séries e muito mais, sem limites </h1>
-        <div className="flex justify-center">  
-          <h1 className="text-lg -mt-12 ms:text-2xl text-center"> Assista onde quiser. Cancele quando quiser. </h1>
-        </div>
-        <div className="flex justify-center">  
-          <h1 className="text-lg ms:text-xl text-center"> Quer assistir? Informe seu email para criar ou reiniciar sua assinatura. </h1>
-        </div>
-        <div className="flex flex-col items-center py-5 ss:flex-row ss:justify-center ss:space-x-4">
-          <input type="text" placeholder="Email" className="bg-[#1b1b1b] opacity-50 rounded border ss:w-[45%] ss:px-5 ss:py-4 w-[80%] px-[10px] py-3 placeholder:text-sm" /> 
-          <button className="bg-[#e50914] rounded border[#e50914] ss:mt-0 ss:py-3 ss:px-5 px-3 py-2 flex items-center mt-4">
-            <span className='font-bold text-lg ss:text-2xl'>Vamos lá</span>
-            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="Hawkins-Icon Hawkins-Icon-Standard"></svg>
-          </button>  
-        </div>
+  <div>    
+    <div className="w-full h-full sm:h-full mt-0 overflow-hidden">
+    <div className="w-10/12 mx-auto">
+    <div className="grid grid-cols-2 gap-4 text-white justify-between py-16 ss:gap-[30px] max-w-[1100px] mx-auto items-center">
+      <div>
+        <p className="text-6xl text-white font-semibold -mt-10"> Iam NoName </p>
+        <p className="text-5xl text-white font-semibold mt-2">
+          <TypeAnimation sequence={work} speed={1} wrapper="span" cursor={true} repeat={Infinity}/>
+        </p>
+      </div>
+      <div className="mt-2 text-center">
+        <Image src="../assets/vercel.svg" alt="Avatar" width={180} height={37} priority className='rounded-full mx-auto' />
       </div>  
+    </div>
+    </div>  
     </div> 
+  </div> 
   )  
 };
 
